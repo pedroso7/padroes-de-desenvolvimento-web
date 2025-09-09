238500efcdececeaa04411b414f2a5a703d69ade
pedroso7 <viniciuspgarcia1@gmail.com>
2025-09-09 01:30:20 +0000

    Aula 08-09-2025 Tests

--- projeto-integrador/src/main/java/entidades/Categoria.java
+++ projeto-integrador/src/main/java/entidades/Categoria.java
@@ -1,5 +1,6 @@
 package entidades;
 
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -7,7 +8,7 @@ import jakarta.persistence.Id;
 // em cima do public class faz com que seja global como agora codigo e nome
 @Entity
 
-public class Categoria {
+public class Categoria extends PanacheEntityBase {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Integer codigo;
